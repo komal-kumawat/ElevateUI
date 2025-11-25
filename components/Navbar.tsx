c2a9b842936e75187bfa172254306bc9aeb78822
komal-kumawat <kumawatkomal17122004@gmail.com>
2025-11-25 18:12:44 +0530

    add it

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,21 +9,9 @@ const Navbar = () => {
         <div className="w-[99.7%] py-4  fixed top-0 backdrop-blur-sm  border-b-1 border-gray-800 z-[100] flex items-center px-20 justify-between ">
             <div className="flex justify-between gap-20">
                 <Link href="/">
-                    <Image src="/logo.svg" alt="logo" width={120} height={40} className="cursor-pointer" />
+                    <Image src="/logoipsum-custom-logo.svg" alt="logo" width={140} height={50} className="cursor-pointer" />
                 </Link>
-                <div className="flex justify-between items-center gap-8 text-sm text-gray-500  hidden md:flex font-bold transition-colors  duration-300">
-                    <Link href="/">
-                        <p className="hover:text-gray-200">Home</p>
-                    </Link>
-                    <Link href="/components">
-                        <p className="hover:text-gray-200">
-                            Components
-                        </p>
-                    </Link>
-                    <Link href="/docs">
-                        <p className="hover:text-gray-200">Docs</p>
-                    </Link>
-                </div>
+
             </div>
             <div className="flex items-center justify-between text-gray-500 gap-8">
                 <Link href="https://github.com/komal-kumawat/ComponentLibrary.git">
