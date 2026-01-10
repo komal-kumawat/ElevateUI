642d0c7ddaae6487ff78b3a9a31a2dc3a34b6ade
komal-kumawat <kumawatkomal17122004@gmail.com>
2026-01-10 21:21:22 +0530

    resolved errors

--- app/component/page.tsx
+++ app/component/page.tsx
@@ -1,28 +1,73 @@
-import Main from '@/app/component/components/Main';
-import Navbar from '@/app/component/components/Navbar';
-import Sidebar from '@/app/component/components/Sidebar';
-import React from 'react';
-
-const Page = () => {
+import Navbar from '@/app/component/components/Navbar'
+import Link from 'next/link'
+import React from 'react'
+import next from "@/Icons/Nextjs.svg"
+import reactIcon from "@/Icons/React.svg";
+import shadcn from "@/Icons/shadcn.svg";
+import Image from 'next/image';
+import tailwind from "@/Icons/tailwindcss.svg"
+import motion from "@/Icons/Motion.svg"
+const page = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-black text-white">
-      {/* Navbar */}
+    <div>
       <Navbar />
+      <div className=' font-bold flex flex-col items-center justify-center h-[100vh] w-full'>
+        <h1 className='text-6xl m-8 text-gray-300'>Build Higher With ElevateUI</h1>
+        <p className='text-xl text-gray-500 '>Craft unique, polished interfaces.
+          A library created to lift your workflow.</p>
+        <div className='flex items-center justify-center m-5'>
+          <Link href="/component/button">
+            <button className='bg-gray-200 p-2 text-gray-900 rounded-md m-5'>
+              browse components
+            </button>
+          </Link>
+          <Link href="/docs">
+            <button className='bg-blue-600 p-2 text-gray-300 rounded-md m-5'>
+              Documentation
+            </button>
+          </Link>
+        </div>
+        <div className='flex gap-8 items-center justify-center'>
+          <div className='flex flex-col items-center justify-center gap-1'>
+            <Link href="">
+              <Image src={next} width={40} alt='next'></Image>
+            </Link>
+            <span>Next</span>
+          </div>
 
-      {/* Content Area */}
-      <div className="flex flex-1 mt-10">
-        {/* Sidebar */}
-        <aside className="hidden md:flex md:w-[30%] lg:w-[20%] h-[100vh] overflow-y-auto border-r border-gray-800">
-          <Sidebar />
-        </aside>
 
-        {/* Main Content */}
-        <main className="w-full md:w-[70%] lg:w-[80%] h-full overflow-y-auto p-4">
-          <Main />
-        </main>
+          <div className='flex flex-col items-center justify-center gap-1'>
+            <Link href="">
+              <Image src={tailwind} width={40} alt='next' className='m-2'></Image>
+            </Link>
+            <span>TailwindCSS</span>
+          </div>
+
+          <div className='flex flex-col items-center justify-center gap-1'>
+            <Link href="">
+              <Image src={motion} width={40} alt='next' className='bg-yellow-400 rounded-4xl p-1'></Image>
+            </Link>
+            <span>Motion</span>
+          </div>
+
+          <div className='flex flex-col items-center justify-center gap-1'>
+            <Link href="">
+              <Image src={shadcn} width={40} alt='next' ></Image>
+            </Link>
+            <span>shadcn/ui</span>
+          </div>
+
+          <div className='flex flex-col items-center justify-center gap-1'>
+            <Link href="">
+              <Image src={reactIcon} width={40} alt='next'></Image>
+            </Link>
+            <span>React</span>
+          </div>
+        </div>
+
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default page
